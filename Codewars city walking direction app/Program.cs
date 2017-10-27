0b4965a1b2e6c386512049bfb4cb322496be1b65
AndrewGit <andrewbryantyeung@gmail.com>
2017-10-27 11:30:39 -0400

    first commit

---
+++ Codewars city walking direction app/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Kata
+{
+    public static bool IsValidWalk(string[] walk)
+    {
+        int west_east_movement = 0;
+        int south_north_movement = 0;
+        if (walk.Length < 10 || walk.Length > 10)
+            return (false);
+
+        else
+        {
+            for (int i = 0; i < walk.Length; i++)
+            {
+                if (walk[i] == "n")
+                {
+                    south_north_movement += 1;
+                }
+                if (walk[i] == "s")
+                {
+                    south_north_movement -= 1;
+                }
+                if (walk[i] == "e")
+                {
+                    west_east_movement += 1;
+                }
+                if (walk[i] == "w")
+                {
+                    west_east_movement -= 1;
+                }
+            }
+
+            if (west_east_movement == 0 && south_north_movement == 0)
+            {
+                return (true);
+            }
+            else
+                return (false);
+
+        }
+    }
+}
